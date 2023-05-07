c8c51fb676d3783252189fc001a6f27ac3b60926
Jason <jasonkodama@gmail.com>
2023-05-07 12:36:48 -0600

    added routes to the htmls

---
+++ Develop/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const path = require('path')
+const notes = require('./db/db.json')
+const PORT = 3001;
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    console.log(__dirname);
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+});
+
+app.get('/notes', (req, res) =>
+res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
+
+
+app.listen(PORT, () =>
+    console.log(`app listening at http://localhost:${PORT}`)
+);
