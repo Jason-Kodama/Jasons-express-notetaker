a00a0dfaaa11ed3f8d8521a361977a2e0c68df3d
Jason <jasonkodama@gmail.com>
2023-05-08 17:59:04 -0600

    added readme

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const app = express();
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use('/api', apiRouter)
+
 app.get('/notes', (req, res) => {
 res.sendFile(path.join(__dirname, './public/notes.html'))
 });
