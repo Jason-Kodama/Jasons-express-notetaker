a991acc9b6bc323ac9c2c807bf695bd5521f2ac7
Jason <jasonkodama@gmail.com>
2023-05-08 21:45:57 -0600

    added heroku port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const apiRouter = require('./routes/apiRoutes')
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.static('public'));
